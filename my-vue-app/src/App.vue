af61dfd6960159cc7b809f88e8c3546393af971c
kneeraazon01 <kneeraazon@gmail.com>
2021-12-14 11:24:52 +0545

    duplicate id fixed

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<!-- Templates -->
+<template>
+  <HelloWorld msg="Home Sweet Home!!" />
+</template>
+
+
+<!-- Styles -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+const count = ref(5)
+</script>
+
+<!--  -->
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count+=5">You biatch clicked me  {{ count }} times already</button>
+</template>
+
+<!--  -->
+<style scoped>
+a {
+  color: #42b983;
+}
+button{
+  background-color: #115c3a;
+  color: #fff;
+  font-size: 24px;
+  padding:6px 16px;
+  border-radius:15px;
+  border:2px solid ;
+}
+</style>
+
+
